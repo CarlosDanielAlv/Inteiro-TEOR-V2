c145165552b4f2a78ee07ba68471e43f755f8693
CarlosDanielAlv <carlosdalves.ads@gmail.com>
2024-09-18 16:14:41 -0300

    debug 3

--- app.py
+++ app.py
@@ -53,10 +53,11 @@ with st.form(key='document_form'):
                 except Exception as e:
                     st.error(f"Erro ao gerar ZIP: {e}")
 
-                # Botão para baixar o arquivo zip
-                st.download_button(
-                    label="Baixar Todos os PDFs",
-                    data=zip_buffer,
-                    file_name="documentos_gerados.zip",
-                    mime="application/zip"
-                )
+                # Oferece o download do arquivo ZIP
+                with open("documentos_gerados.zip", "rb") as f:
+                    st.download_button(
+                        label="Baixar Todos os PDFs",
+                        data=zip_buffer,
+                        file_name="documentos_gerados.zip",
+                        mime="application/zip"
+                    )
