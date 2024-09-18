02200a2cafe77e78a9f690ee06d8f8f94b64154f
CarlosDanielAlv <carlosdalves.ads@gmail.com>
2024-09-18 15:42:25 -0300

    Ajuste botão submit

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from services.file_converter import zip_pdfs  # Importando a função de zipar P
 # Cabeçalho da aplicação
 st.title('Gerador de Documentos e Conversor para PDF')
 
+# Upload do arquivo Excel
+uploaded_excel = st.file_uploader("Carregar arquivo Excel", type=["xlsx"])
+
 # Organizar todos os componentes dentro de um formulário
 with st.form(key='document_form'):
-    # Upload do arquivo Excel
-    uploaded_excel = st.file_uploader("Carregar arquivo Excel", type=["xlsx"])
-
     # Verifica se o arquivo foi carregado
     if uploaded_excel is not None:
         # Converte o arquivo Excel carregado em um DataFrame do Pandas
@@ -26,29 +26,31 @@ with st.form(key='document_form'):
         selected_template = st.multiselect("Selecione os modelos de documento", list(
         template_choices.keys()), placeholder="Selecione um ou mais modelos")
 
-        # Botão para gerar o documento
-        if st.button("Gerar Documento"):
-            if not selected_template:
-                st.warning("Selecione pelo menos um modelo.")
-            else:
-                # Exibe um spinner enquanto os documentos estão sendo gerados
-                with st.spinner('Gerando documentos...'):
-                    # Chama a função para gerar os documentos passando o DataFrame lido
-                    template_paths = [template_choices[modelo]
-                                for modelo in selected_template]
-                    
-                    documentos_gerados = generate_documents(df, template_paths)
+    # Botão para enviar o formulário (submit)
+    submit_button = st.form_submit_button("Gerar Documentos")
 
-                    # Converter documentos gerados para PDF e zipá-los
-                    pdf_files = [docx_file.replace('.docx', '.pdf') for docx_file in documentos_gerados]
+    if submit_button and uploaded_excel is not None:
+        if not selected_template:
+            st.warning("Selecione pelo menos um modelo.")
+        else:
+            # Exibe um spinner enquanto os documentos estão sendo gerados
+            with st.spinner('Gerando documentos...'):
+                template_paths = [template_choices[modelo]
+                                  for modelo in selected_template]
+                
+                # Chama a função para gerar os documentos passando o DataFrame lido
+                documentos_gerados = generate_documents(df, template_paths)
 
-                    # Zipar os PDFs usando a função no file_converter
-                    zip_buffer = zip_pdfs(pdf_files)
+                # Converter documentos gerados para PDF e zipá-los
+                pdf_files = [docx_file.replace('.docx', '.pdf') for docx_file in documentos_gerados]
 
-                    # Botão para baixar o arquivo zip
-                    st.download_button(
-                        label="⬇️ Baixar Arquivos",
-                        data=zip_buffer,
-                        file_name="documentos_gerados.zip",
-                        mime="application/zip"
-                    )
+                # Zipar os PDFs usando a função no file_converter
+                zip_buffer = zip_pdfs(pdf_files)
+
+                # Botão para baixar o arquivo zip
+                st.download_button(
+                    label="Baixar Todos os PDFs",
+                    data=zip_buffer,
+                    file_name="documentos_gerados.zip",
+                    mime="application/zip"
+                )
