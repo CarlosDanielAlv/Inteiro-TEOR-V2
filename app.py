8c1a063c74ac4247cebc3c17d6df2eff2c225bcc
CarlosDanielAlv <carlosdalves.ads@gmail.com>
2024-09-18 21:47:29 -0300

    ux 1

--- app.py
+++ app.py
@@ -1,13 +1,47 @@
 # app.py
-
 import streamlit as st
 import pandas as pd
 from services.doc_processor import generate_documents
 from services.file_converter import zip_pdfs  # Importando a função de zipar PDFs
+from PIL import Image
 
 
 # Cabeçalho da aplicação
-st.title('Gerador de Documentos e Conversor para PDF')
+favicon = Image.open("favicon.png")  # Pode ser .ico, .png, .jpg, etc.
+
+st.set_page_config(
+    page_title="Gerador de Inteiro TEOR",
+    page_icon=favicon,  # Favicon personalizado
+    layout="centered"
+)
+
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB; color: #fff;">
+  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Data Professor</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://youtube.com/dataprofessor" target="_blank">YouTube</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://twitter.com/thedataprof" target="_blank">Twitter</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+""", unsafe_allow_html=True)
+
+st.markdown('''# **Gerador de Inteiro TEOR**
+Gere Inteiro TEOR DA R1 e R2.
+''')
 
 # Upload do arquivo Excel
 uploaded_excel = st.file_uploader("Carregar arquivo Excel", type=["xlsx"])
@@ -65,3 +99,31 @@ if zip_buffer:
         file_name="documentos_gerados.zip",
         mime="application/zip"
     )
+
+
+# Adicionar o footer com HTML e CSS embutido
+footer = """
+    <style>
+        .footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #262730;
+            color: #fff;
+            text-align: center;
+            padding: 10px;
+            font-size: 14px;
+        }
+    </style>
+    <div class="footer">
+        <p>© 2024 - Todos os direitos reservados | Desenvolvido por Carlos Daniel da Silva Alves</p>
+    </div>
+"""
+st.markdown(footer, unsafe_allow_html=True)
+
+st.markdown("""
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+""", unsafe_allow_html=True)
